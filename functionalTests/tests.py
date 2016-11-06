9684adb0ad1f2fe8b394dbc479577af57c959a1a
lorilew <lorilew123@gmail.com>
2016-11-06 16:30:21 +0000

    Updated home template

--- functionalTests/tests.py
+++ functionalTests/tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-do', header.text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new-item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual('Enter to-do item here.', inputbox.get_attribute('placeholder'))
 
         # She types 'Buy peacock feathers' into a text box.
@@ -42,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly'
-        inputbox = self.browser.find_element_by_id('id_new-item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual('Enter to-do item here.', inputbox.get_attribute('placeholder'))
 
         inputbox.send_keys('Use peacock feathers to make a fly')
